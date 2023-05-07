7ec2240605332e7417a63de39d33389cd663e9f8
Siarhei_Shpakouski <siarhei_shpakouski@epam.com>
2023-05-08 02:36:14 +0400

    all done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,17 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int revertedNumber = 0;
+        int modulus;
+
+        while(number > 0) {
+            revertedNumber *= 10;
+
+            modulus = number % 10;
+            number /= 10;
+
+            revertedNumber += modulus;
+        }
+        System.out.println(revertedNumber);
     }
 }
